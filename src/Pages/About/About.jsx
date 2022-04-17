030b4144f3ac12d91f29f104930c093a5c7c499e
DevTabibur <tobiburrohman2@gmail.com>
2022-04-17 16:04:14 +0600

    updated img about

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.css';
 import {Container, Row, Col} from 'react-bootstrap';
-import AboutImg from '../../Assets/Images/about.jpg';
+import AboutImg from '../../Assets/Images/about.png';
 import Signature from '../../Assets/Images/signature.png';
 import {GoHome} from 'react-icons/go'
 
@@ -17,42 +17,49 @@ const About = () => {
         </Container>
       </div>
 
+      
+
       <div className="about-section py-5">
         <Container>
           <Row>
 
-            <Col md={4} sm={12}>
+            <Col md={5} sm={12}>
               <div className="left-col">
                 <div className="section-title">
                   <div className="icon">
                     <GoHome className='go-home-icon'/>
                   </div>
-                  <span>About Juristic</span>
-                  <h2>I'm The Most Popular Lawyer With Various Law Services!</h2>
+                  <span>About Me</span>
+                  <h2>Tabibur Rahman</h2>
                   <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum</p>
                 </div>
               </div>
+              <div className="right-col">
+                <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien libero</p>
+                <div className="quoter">
+                  <h4>Tabibur Rahman</h4>
+                  <span>- Learner at Programming hero</span>
+                </div>
+              </div>
             </Col>
+            <Col md={2}></Col>
 
-            <Col md={4} sm={12}>
+            <Col md={5} sm={12}>
             <div className="mid-col">
               <img src={AboutImg} alt="AboutImg" />
             </div>
             </Col>
 
-            <Col md={4} sm={12}>
+            {/* <Col md={4} sm={12}>
               <div className="right-col">
                 <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien libero</p>
                 <div className="quoter">
-                  <h4>Juristic John Dow</h4>
-                  <span>- Lawyer of the US Court</span>
-                </div>
-                <div className="signature">
-                  <img src={Signature} alt="Signature" />
+                  <h4>Tabibur Rahman</h4>
+                  <span>- Learner at Programming hero</span>
                 </div>
               </div>
 
-            </Col>
+            </Col> */}
 
           </Row>
         </Container>
