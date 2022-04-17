b0ec6abd1c09b51aa259bc4ddfb1e52e2c5a0e8e
DevTabibur <tobiburrohman2@gmail.com>
2022-04-17 15:01:49 +0600

    about page working here

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './About.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="about-section">
+        <Container>
+          <Row>
+            <Col>
+              <h2>Hey it's me</h2>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   )
 }
 
