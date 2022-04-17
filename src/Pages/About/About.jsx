1a8b0e96868c204e2398d71607ab3f9c8542401c
DevTabibur <tobiburrohman2@gmail.com>
2022-04-17 16:21:06 +0600

    all done about page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './About.css';
-import {Container, Row, Col} from 'react-bootstrap';
-import AboutImg from '../../Assets/Images/about.png';
-import Signature from '../../Assets/Images/signature.png';
-import {GoHome} from 'react-icons/go'
+import React from "react";
+import "./About.css";
+import { Container, Row, Col } from "react-bootstrap";
+import AboutImg from "../../Assets/Images/about.png";
+import Signature from "../../Assets/Images/signature.png";
+import { GoHome } from "react-icons/go";
 
 const About = () => {
   return (
     <>
-    <div className="page-title shadow">
+      <div className="page-title shadow">
         <Container className="py-5">
           <Row>
             <h2>About Me</h2>
@@ -17,25 +17,28 @@ const About = () => {
         </Container>
       </div>
 
-      
-
       <div className="about-section py-5">
         <Container>
           <Row>
-
             <Col md={5} sm={12}>
               <div className="left-col">
                 <div className="section-title">
                   <div className="icon">
-                    <GoHome className='go-home-icon'/>
+                    <GoHome className="go-home-icon" />
                   </div>
                   <span>About Me</span>
                   <h2>Tabibur Rahman</h2>
-                  <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum</p>
                 </div>
               </div>
               <div className="right-col">
-                <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien libero</p>
+                <p>
+                  I want to be a professional web developer at 2022 Insha-Allah.
+                  My life is a very simple. My father has been died when I was
+                  only two. My first and only first aim is that to be a MERN
+                  stack developer. And get a job at 2022 Insha-Allah. For
+                  getting a job, I worked hard with Smart way. Best Wishes for
+                  me.
+                </p>
                 <div className="quoter">
                   <h4>Tabibur Rahman</h4>
                   <span>- Learner at Programming hero</span>
@@ -44,28 +47,16 @@ const About = () => {
             </Col>
             <Col md={2}></Col>
 
-            <Col md={5} sm={12}>
-            <div className="mid-col">
-              <img src={AboutImg} alt="AboutImg" />
-            </div>
-            </Col>
-
-            {/* <Col md={4} sm={12}>
-              <div className="right-col">
-                <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien libero</p>
-                <div className="quoter">
-                  <h4>Tabibur Rahman</h4>
-                  <span>- Learner at Programming hero</span>
-                </div>
+            <Col md={5} sm={12} order="1">
+              <div className="mid-col">
+                <img src={AboutImg} alt="AboutImg" />
               </div>
-
-            </Col> */}
-
+            </Col>
           </Row>
         </Container>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
