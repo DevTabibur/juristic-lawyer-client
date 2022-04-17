39cca2366eaee48b2912ef6b26df69ffc795f82a
DevTabibur <tobiburrohman2@gmail.com>
2022-04-18 02:17:59 +0600

    banner res done mobile s

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <>
       <HeroBanner/>
-      <Service/>
+      {/* <Service/> */}
     </>
   )
 }
--- src/Pages/Shared/HeroBanner/HeroBanner.jsx
+++ src/Pages/Shared/HeroBanner/HeroBanner.jsx
@@ -17,9 +17,13 @@ import Slider1 from '../../../Assets/Images/slide-1.jpg'
 import Slider2 from '../../../Assets/Images/slide-2.jpg'
 import Slider3 from '../../../Assets/Images/slide-3.jpg'
 import Slider4 from '../../../Assets/Images/slide-4.jpg'
+import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
   SwiperCore.use([Autoplay]);
+  const navigate = useNavigate();
+
+  
 
  
   return (
@@ -30,10 +34,10 @@ const HeroBanner = () => {
         slidesPerView={1}
         // spaceBetween={8}
         loop={true}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //   delay: 4000,
+        //   disableOnInteraction: false,
+        // }}
         className="mySwiper pb-5"
       >
         <SwiperSlide className="swiper-slider-parent">
@@ -44,8 +48,9 @@ const HeroBanner = () => {
                 <button className="slider-read-btn">FREE CONSULTATION</button>
             </div>
         </SwiperSlide>
+
         <SwiperSlide className="swiper-slider-parent">
-            <img src={Slider1} alt="Slider1" />
+            <img src={Slider2} alt="Slider2" />
             <div className="slider-inner-info">
                 <h1 className="mb-3">Our Independence Make the Difference</h1>
                 <h4 className="mb-3">NATIONALLY ESTABLISHED. INTERNATIONALLY RECOGNIZED</h4>
@@ -53,7 +58,7 @@ const HeroBanner = () => {
             </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slider-parent">
-            <img src={Slider2} alt="Slider1" />
+            <img src={Slider3} alt="Slider3" />
             <div className="slider-inner-info">
                 <h1 className="mb-3">Our Independence Make the Difference</h1>
                 <h4 className="mb-3">NATIONALLY ESTABLISHED. INTERNATIONALLY RECOGNIZED</h4>
@@ -61,21 +66,14 @@ const HeroBanner = () => {
             </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slider-parent">
-            <img src={Slider3} alt="Slider1" />
+            <img src={Slider4} alt="Slider4" />
             <div className="slider-inner-info">
                 <h1 className="mb-3">Our Independence Make the Difference</h1>
                 <h4 className="mb-3">NATIONALLY ESTABLISHED. INTERNATIONALLY RECOGNIZED</h4>
                 <button className="slider-read-btn">FREE CONSULTATION</button>
             </div>
         </SwiperSlide>
-        <SwiperSlide className="swiper-slider-parent">
-            <img src={Slider4} alt="Slider1" />
-            <div className="slider-inner-info">
-                <h1 className="mb-3">Our Independence Make the Difference</h1>
-                <h4 className="mb-3">NATIONALLY ESTABLISHED. INTERNATIONALLY RECOGNIZED</h4>
-                <button className="slider-read-btn">FREE CONSULTATION</button>
-            </div>
-        </SwiperSlide>
+        
 
       </Swiper>
     </>
