e2b11d9214c69c5f0e42a0940a8df43116e83324
DevTabibur <tobiburrohman2@gmail.com>
2022-04-17 14:50:13 +0600

    sigup page done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -23,58 +23,60 @@ const Login = () => {
         </Container>
       </div>
 
-      <Container className="py-5">
-        <Row>
-          <Col></Col>
-          <Col sm={4}>
-            <div className="form-bg shadow rounded-3 p-3">
-              <h2 className="title text-center my-2">
-                Login <span className="title-2">Form</span>
-              </h2>
-              <Form onSubmit={handleFormSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
+      <div className="login-bg">
+        <Container className="py-5">
+          <Row>
+            <Col></Col>
+            <Col sm={4}>
+              <div className="form-bg shadow rounded-3 p-3">
+                <h2 className="title text-center my-2">
+                  Login <span className="title-2">Form</span>
+                </h2>
+                <Form onSubmit={handleFormSubmit}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button type="submit" className="w-100 d-block login-btn">
-                  LOGIN
-                </Button>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  <Button type="submit" className="w-100 d-block login-btn">
+                    LOGIN
+                  </Button>
 
-                <div className="my-3 d-flex justify-content-center form-bottom-text">
-                  <h6 className="text-dark ">
-                    New to Juristic?{" "}
-                    <Link to="/register"> Create an Account</Link>
-                  </h6>
-                </div>
+                  <div className="my-3 d-flex justify-content-center form-bottom-text">
+                    <h6 className="text-dark ">
+                      New to Juristic?{" "}
+                      <Link to="/register"> Create an Account</Link>
+                    </h6>
+                  </div>
 
-                <div className="or-text d-flex align-items-center justify-content-center">
-                  <span>--------</span>
-                  <h6 className="mt-2 mx-4">
-                    <span>OR</span>
-                  </h6>
-                  <span>--------</span>
-                </div>
+                  <div className="or-text d-flex align-items-center justify-content-center">
+                    <span>--------</span>
+                    <h6 className="mt-2 mx-4">
+                      <span>OR</span>
+                    </h6>
+                    <span>--------</span>
+                  </div>
 
-                <div className="logo-wrapper w-100">
-                  <button className="google-auth" onClick={googleAuth}>
-                    <img src={GoogleLogo} alt="google__logo" />
-                    <p> Continue with Google </p>
-                  </button>
-                </div>
-              </Form>
-            </div>
-          </Col>
-          <Col></Col>
-        </Row>
-      </Container>
+                  <div className="logo-wrapper w-100">
+                    <button className="google-auth" onClick={googleAuth}>
+                      <img src={GoogleLogo} alt="google__logo" />
+                      <p> Continue with Google </p>
+                    </button>
+                  </div>
+                </Form>
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,10 +1,91 @@
-import React from 'react';
-import './Register.css';
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import GoogleLogo from "../../Assets/Icons/google (1).svg";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const googleAuth = () => {
+    alert();
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default Register
+  return (
+    <>
+      <div className="page-title shadow">
+        <Container className="py-5">
+          <Row>
+            <h2>Register Here</h2>
+            <p>Gregor then turned to look out the window at the weather</p>
+          </Row>
+        </Container>
+      </div>
+
+      <div className="login-bg">
+        <Container className="py-5">
+          <Row>
+            <Col></Col>
+            <Col sm={4}>
+              <div className="form-bg shadow rounded-3 p-3">
+                <h2 className="title text-center my-2">
+                  Register <span className="title-2">Form</span>
+                </h2>
+
+                <Form onSubmit={handleFormSubmit}>
+
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                      We'll never share your email with anyone else.
+                    </Form.Text>
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Confirm Password" />
+                  </Form.Group>
+
+                  <Button type="submit" className="w-100 d-block login-btn">
+                    REGISTER
+                  </Button>
+
+                  <div className="my-3 d-flex justify-content-center form-bottom-text">
+                    <h6 className="text-dark ">
+                      New to Juristic?{" "}
+                      <Link to="/register"> Create an Account</Link>
+                    </h6>
+                  </div>
+
+                  <div className="or-text d-flex align-items-center justify-content-center">
+                    <span>--------</span>
+                    <h6 className="mt-2 mx-4">
+                      <span>OR</span>
+                    </h6>
+                    <span>--------</span>
+                  </div>
+
+                  <div className="logo-wrapper w-100">
+                    <button className="google-auth" onClick={googleAuth}>
+                      <img src={GoogleLogo} alt="google__logo" />
+                      <p> Continue with Google </p>
+                    </button>
+                  </div>
+                </Form>
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Register;
