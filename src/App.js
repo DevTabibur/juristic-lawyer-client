f4ec04284fb9a1e1dfabddb786cc0e5c0646b519
DevTabibur <tobiburrohman2@gmail.com>
2022-04-18 19:59:47 +0600

    banner res ok

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         }></Route>
         <Route path="/*" element={<NotFound />}></Route>
       </Routes>
-      <Footer/>
+      {/* <Footer/> */}
     </>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
   return (
     <>
       <HeroBanner/>
-      <Service/>
-      <Extra/>
+      {/* <Service/>
+      <Extra/> */}
     </>
   )
 }
