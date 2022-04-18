666d3e470c594a562ab662548257ab3031895885
DevTabibur <tobiburrohman2@gmail.com>
2022-04-18 10:48:17 +0600

    footer pb solved

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import RequireAuth from "./Pages/Shared/RequireAuth/RequireAuth";
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/home" element={<Home />}></Route>
